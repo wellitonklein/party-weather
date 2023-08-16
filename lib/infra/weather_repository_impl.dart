aae246b149796bbc236cf29d85581457f76f9d07
Welliton Klein <welliton.fokushima@gmail.com>
2023-08-16 15:56:51 -0300

    feat: Criação do repositório para consultar o clima por lon e lat

---
+++ lib/infra/weather_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:uno/uno.dart';
+
+import '../domain/domain.dart';
+import 'mappers/mappers.dart';
+
+class WeatherRepositoryImpl implements WeatherRepository {
+  final Uno client;
+  final String url;
+  final String appId;
+
+  const WeatherRepositoryImpl({
+    required this.client,
+    required this.url,
+    required this.appId,
+  });
+
+  @override
+  Future<WeatherEntity> searchByLocation({
+    required double lon,
+    required double lat,
+  }) async {
+    final response = await client.get(
+      '$url/data/2.5/weather?lat=$lat&lon=$lon&APPID=$appId&units=metric',
+    );
+
+    if (response.status == 200) {
+      return WeatherMapper.fromMap(response.data);
+    }
+
+    throw Exception('Não foi encontrado nenhuma previsão.');
+  }
+
+  @override
+  Future<WeatherEntity> searchByGeolocation() async {
+    // TODO: implement searchByGeolocation
+    throw UnimplementedError();
+  }
+}
