9b857e668e2e086e497646aac8860678538985e7
Welliton Klein <welliton.fokushima@gmail.com>
2023-08-16 15:20:43 -0300

    feat: desenho da consulta de cidades

--- lib/presentation/ui/pages/list_of_cities/list_of_cities_page.dart
+++ lib/presentation/ui/pages/list_of_cities/list_of_cities_page.dart
@@ -1,66 +1,82 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:party_weather/domain/domain.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ListOfCitiesPage extends StatelessWidget {
+import '../../../../main/factories/factories.dart';
+import '../../../bloc/bloc.dart';
+import 'widgets/widgets.dart';
+
+class ListOfCitiesPage extends StatefulWidget {
   const ListOfCitiesPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final cities = List.generate(
-      0,
-      (index) => CityEntity(
-        name: faker.address.city(),
-        state: faker.address.state(),
-        country: faker.address.countryCode(),
-        lon: 342432.234,
-        lat: 1231231.77,
-      ),
-    );
+  State<ListOfCitiesPage> createState() => _ListOfCitiesPageState();
+}
 
+class _ListOfCitiesPageState extends State<ListOfCitiesPage> {
+  late final ListOfCitiesBloc bloc;
+
+  @override
+  void initState() {
+    bloc = listOfCitiesBlocFactory();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    bloc.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de cidades'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Digite o nome da cidade',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            if (cities.isNotEmpty)
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: cities.length,
-                  itemBuilder: (context, index) {
-                    final city = cities[index];
+        child: BlocBuilder<ListOfCitiesBloc, ListOfCitiesState>(
+          bloc: bloc,
+          builder: (context, state) {
+            void restartPage() {
+              bloc.add(ListOfCitiesRestarted());
+            }
 
-                    return ListTile(
-                      onTap: () {},
-                      title: Text(city.name),
-                      subtitle: Text('${city.state} - ${city.country}'),
-                      trailing: Image.network(
-                        'https://openweathermap.org/images/flags/${city.country.toLowerCase()}.png',
-                        errorBuilder: (context, error, stackTrace) {
-                          return const Icon(Icons.not_accessible);
-                        },
-                      ),
-                    );
-                  },
+            void searchText(String value) {
+              bloc.add(ListOfCitiesFieldChanged(value: value));
+            }
+
+            void searched() {
+              bloc.add(ListOfCitiesSearched());
+            }
+
+            if (state is ListOfCitiesInitial) {
+              return Center(
+                child: SearchFieldWidget(
+                  onChanged: searchText,
+                  onPressed: searched,
                 ),
-              ),
-          ],
+              );
+            }
+
+            if (state is ListOfCitiesLoading) {
+              return const CityLoadingWidget();
+            }
+
+            if (state is ListOfCitiesNotFound) {
+              return CityNotFoundWidget(restartPage: restartPage);
+            }
+
+            if (state is ListOfCitiesDataFound) {
+              return CityListWidget(
+                cities: state.cities,
+                onChanged: searchText,
+                onPressed: searched,
+              );
+            }
+
+            return const SizedBox.shrink();
+          },
         ),
       ),
     );
