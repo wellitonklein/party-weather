f1c2c9cea65f9262754d690c89cb0a84b22ce833
Welliton Klein <welliton.fokushima@gmail.com>
2023-08-18 17:08:17 -0300

    refactor: refatorando cidades

--- lib/infra/city_repository_impl.dart
+++ lib/infra/city_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:uno/uno.dart';
 
 import '../domain/domain.dart';
@@ -25,5 +26,41 @@ class CityRepositoryImpl implements CityRepository {
     }
 
     throw Exception('Não foi possível encontrar a cidade.');
+  }
+
+  @override
+  Future<CityEntity> searchByGeolocation() async {
+    var serviceEnabled = false;
+    late LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('GPS está desativado.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception(
+          'Precisar dar permissão para encontrar sua localização.',
+        );
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+        'Permissão de localização negada permanentemente.',
+      );
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    return CityEntity.init(
+      name: '',
+      state: '',
+      country: '',
+      lon: position.longitude,
+      lat: position.latitude,
+    );
   }
 }
